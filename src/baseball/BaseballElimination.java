c1fac623183f0250fb8535d644dde4d50533c831
gatordan82 <gatordan82@gmail.com>
2016-05-13 17:43:34 -0400

    Refactored team data
    Removed Team class in favor of int arrays for wins/losses/remaining, and
    hash tables for teams and remaining games against structures.

--- src/baseball/BaseballElimination.java
+++ src/baseball/BaseballElimination.java
@@ -3,12 +3,8 @@
  */
 package baseball;
 
-import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.LinearProbingHashST;
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.ST;
-import edu.princeton.cs.algs4.StdOut;
 
 /**
  * @author Dan
@@ -17,47 +13,65 @@ import edu.princeton.cs.algs4.StdOut;
 public class BaseballElimination
 {
 	private int N;
-	private String[] teamNames;
-	private LinearProbingHashST<String, Team> teams;
+	private LinearProbingHashST<String, Integer> teamNames;
+	private int[] wins;
+	private int[] losses;
+	private int[] remaining;
+	private LinearProbingHashST<String, Integer[]> against;
 	
-	private class Team
-	{
-		private int wins;
-		private int losses;
-		private int remaining;
-		private LinearProbingHashST<String, Integer> against;
-		
-		private Team(String[] schedule, String[] teams)
-		{
-			wins      = Integer.parseInt(schedule[1]);
-			losses    = Integer.parseInt(schedule[2]);
-			remaining = Integer.parseInt(schedule[3]);
-			
-			against = new LinearProbingHashST<String, Integer>();
-			
-			for (int i = 0; i < N; i++)
-				against.put(teams[i], Integer.parseInt(schedule[i + 4]));
-		}
-	}
+//	private class Team
+//	{
+//		private int wins;
+//		private int losses;
+//		private int remaining;
+//		private LinearProbingHashST<String, Integer> against;
+//		
+//		private Team(String[] schedule, String[] teams)
+//		{
+//			wins      = Integer.parseInt(schedule[1]);
+//			losses    = Integer.parseInt(schedule[2]);
+//			remaining = Integer.parseInt(schedule[3]);
+//			
+//			against = new LinearProbingHashST<String, Integer>();
+//			
+//			for (int i = 0; i < N; i++)
+//				against.put(teams[i], Integer.parseInt(schedule[i + 4]));
+//		}
+//	}
 	
 	public BaseballElimination(String filename)
 	{
 		In in = new In(filename);
 		N = Integer.parseInt(in.readLine());
-		String[][] statLines = new String[N][N-2];
 		
-		teamNames = new String[N];
-		int j = 0;
+		teamNames = new LinearProbingHashST<String, Integer>();
+		wins      = new int[N];
+		losses    = new int[N];
+		remaining = new int[N];
+		against   = new LinearProbingHashST<String, Integer[]>();
+		
+		int i = 0;
+		String[] statLine = new String[N + 4];
+		
 		while (!in.isEmpty())
 		{
-			statLines[j] = in.readLine().trim().split("\\s+");
-			teamNames[j] = statLines[j][0];
-			j++;
+			statLine = in.readLine().trim().split("\\s+");
+			teamNames.put(statLine[0], i);
+			wins[i]      = Integer.parseInt(statLine[1]);
+			losses[i]    = Integer.parseInt(statLine[2]);
+			remaining[i] = Integer.parseInt(statLine[3]);
+			
+			Integer[] againstArray = new Integer[N];
+			for (int j = 0; j < N; j++)
+				againstArray[j] = Integer.parseInt(statLine[j + 4]);
+			against.put(statLine[0], againstArray);
+			
+			i++;
 		}
 		
-		teams = new LinearProbingHashST<String, Team>();
-		for (int i = 0; i < N; i++)
-			teams.put(teamNames[i], new Team(statLines[i], teamNames));
+//		teams = new LinearProbingHashST<String, Team>();
+//		for (int i = 0; i < N; i++)
+//			teams.put(teamNames[i], new Team(statLines[i], teamNames));
 	}
 	
 	public int numberOfTeams()
@@ -67,27 +81,33 @@ public class BaseballElimination
 	
 	public Iterable<String> team()
 	{
-		return teams.keys();
+		return against.keys();
+	}
+	
+	private int getTeamIndex(String team)
+	{
+		return teamNames.get(team);
 	}
 	
 	public int wins(String team)
 	{
-		return teams.get(team).wins;
+		return wins[getTeamIndex(team)];
 	}
 	
 	public int losses(String team)
 	{
-		return teams.get(team).losses;
+		return losses[getTeamIndex(team)];
 	}
 	
 	public int remaining(String team)
 	{
-		return teams.get(team).remaining;
+		return remaining[getTeamIndex(team)];
 	}
 	
 	public int against(String team1, String team2)
 	{
-		return teams.get(team1).against.get(team2);
+		int team2Index = getTeamIndex(team2);
+		return against.get(team1)[team2Index];
 	}
 	
 //	public boolean isEliminated(String team)
@@ -109,7 +129,7 @@ public class BaseballElimination
 		BaseballElimination division = new BaseballElimination(args[0]);
 		for (String team : division.team())
 		{
-			System.out.println(division.wins(team));
+			System.out.println(team + " has " + division.wins(team) + " wins.");
 //			if (division.isEliminated(team))
 //			{
 //				StdOut.print(team + " is eliminated by the subset R = { ");
