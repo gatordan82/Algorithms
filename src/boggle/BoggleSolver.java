e90592c19f297bedc2e43cd5248b9eef122764c2
Dan-NextInput <dan.benjamin@nextinput.com>
2016-05-18 23:57:11 -0400

    Added starter files for boggle
    Imported Boggle board, and blank boggle solver classes.

---
+++ src/boggle/BoggleSolver.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package boggle;
+
+/**
+ * @author Dan
+ *
+ */
+public class BoggleSolver
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
