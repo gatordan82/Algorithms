c7a51245ee191f0df9a67d7023fa193ac8f6aadf
Dan-NextInput <dan.benjamin@nextinput.com>
2016-05-29 21:18:54 -0400

    Completed debugging boggle
    Fixed issue with changing row/column size, and in dfs when the node was
    null.

--- src/boggle/BoggleSolver.java
+++ src/boggle/BoggleSolver.java
@@ -10,6 +10,41 @@ package boggle;
 public class BoggleSolver
 {
 
+	public BoggleSolver(String[] dictionary)
+	{
+		
+	}
+	
+	public Iterable<String> getAllValidWords(BoggleBoard board)
+	{
+		
+	}
+	
+	public int scoreOf(String word)
+	{
+		int length = word.length();
+		int score;
+		
+		switch (length)
+		{
+			case 0: case 1: case 2:
+				score = 0;
+			case 3: case 4:
+				score = 1;
+			case 5:
+				score = 2;
+			case 6:
+				score = 3;
+			case 7:
+				score = 5;
+			default:
+				score = 11;
+		}
+			
+		return score;
+	}
+	
+	
 	/**
 	 * @param args
 	 */
